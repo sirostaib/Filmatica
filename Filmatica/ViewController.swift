7552f9d8a8f8f0d77451e82cb9f44df457959a1f
Caesar <sirostaib@gmail.com>
2024-07-08 02:55:20 +0300

    initial commit
    starter project

---
+++ Filmatica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Filmatica
+//
+//  Created by Siros Taib on 7/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
