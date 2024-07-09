86657c26c192fd28142483e9ffbc7231988be789
Caesar <sirostaib@gmail.com>
2024-07-09 11:03:16 +0300

    added Support folder

--- Filmatica/Support/SceneDelegate.swift
+++ Filmatica/Support/SceneDelegate.swift
@@ -11,22 +11,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
+
         let navVC = UINavigationController()
         let coordinator = MainCoordinator()
-        
+
         coordinator.navigationController = navVC
-        
+
         let window = UIWindow(windowScene: windowScene)
-        
+
         window.rootViewController = navVC
         window.makeKeyAndVisible()
         self.window = window
-        
+
         coordinator.start()
     }
 
@@ -34,7 +34,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        // The scene may re-connect later, as its session was not necessarily discarded 
+        // (see `application:didDiscardSceneSessions` instead).
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
@@ -58,6 +59,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
-
 }
-
