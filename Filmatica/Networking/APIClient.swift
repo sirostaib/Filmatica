eed49ccd50b950365b15e1451aada87d7723b32f
Caesar <sirostaib@gmail.com>
2024-07-10 10:49:24 +0300

    added networking file
    empty for now

---
+++ Filmatica/Networking/APIClient.swift
@@ -0,0 +1,8 @@
+//
+//  APIClient.swift
+//  Filmatica
+//
+//  Created by Siros Taib on 7/10/24.
+//
+
+import Foundation
---
+++ Filmatica/Networking/NetworkConstant.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkConstant.swift
+//  Filmatica
+//
+//  Created by Siros Taib on 7/10/24.
+//
+
+import Foundation
