f378ffda1bec43166c36691c5cf1f0e0bff60ea8
Caesar <sirostaib@gmail.com>
2024-07-11 09:59:07 +0300

    modified movie model

--- Filmatica/Models/Movie.swift
+++ Filmatica/Models/Movie.swift
@@ -7,8 +7,41 @@
 
 import Foundation
 
-struct Movie {
-    let title: String
-    let releaseDate: String
-    let overview: String
+// MARK: - MoviesModel - from JSON
+struct MoviesModel: Codable {
+    let page: Int
+    let results: [Movie]
+    let totalPages, totalResults: Int
+
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+// MARK: - Movie Result Model
+struct Movie: Codable, Equatable {
+    let id: Int
+    let originalTitle: String?
+    let overview, posterPath: String
+    let title: String?
+    let popularity: Double
+    let releaseDate: String?
+    let voteAverage: Double
+    let originalName, name, firstAirDate: String?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case originalTitle = "original_title"
+        case overview
+        case posterPath = "poster_path"
+        case title
+        case popularity
+        case releaseDate = "release_date"
+        case voteAverage = "vote_average"
+        case originalName = "original_name"
+        case name
+        case firstAirDate = "first_air_date"
+    }
 }
