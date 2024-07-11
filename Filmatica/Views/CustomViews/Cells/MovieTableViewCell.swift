accf6749ba84ac70f8f034fab7b0d7a5acbfe48d
Caesar <sirostaib@gmail.com>
2024-07-11 10:00:02 +0300

    modified movie list custom cell

--- Filmatica/Views/CustomViews/Cells/MovieTableViewCell.swift
+++ Filmatica/Views/CustomViews/Cells/MovieTableViewCell.swift
@@ -10,74 +10,64 @@ import Kingfisher
 
 class MovieTableViewCell: UITableViewCell {
 
+    // MARK: - Properties
     static let identifier = "MovieTableViewCell"
 
-    private let movieImage = FImageView(frame: CGRect())
+    private let posterImageView = FImageView(frame: CGRect())
     private let titleLabel = FTitleLabel(fontSize: 22)
     private let secondaryLabel = FBodyLabel(fontSize: 16)
     private var labelStackView = FStackView()
 
+    // MARK: - Dependency Injection Init
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.addSubview(movieImage)
-        setupStackUI()
-        contentView.addSubview(labelStackView)
-//        contentView.addSubview(titleLabel)
-//        contentView.addSubview(secondaryLabel)
 
-        setupConstraints()
+        setupPosterViewUI()
+        setupStackViewUI()
         setupCellUI()
+
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    private func setupStackUI() {
-        labelStackView = FStackView(arrangedSubviews: [titleLabel, secondaryLabel])
+    // MARK: - Setup UI layout
+    private func setupPosterViewUI() {
+        contentView.addSubview(posterImageView)
+
+        NSLayoutConstraint.activate([
+            posterImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            posterImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
+            posterImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            posterImageView.widthAnchor.constraint(equalToConstant: 100)
+        ])
+
+        let heightConstraint = posterImageView.heightAnchor.constraint(equalToConstant: 150)
+        heightConstraint.priority = UILayoutPriority(999)
+        heightConstraint.isActive = true
     }
 
-    private func setupConstraints() {
-        NSLayoutConstraint.activate([
-            // Movie Image Constraints
-            movieImage.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
-            movieImage.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
-            movieImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
-            movieImage.heightAnchor.constraint(equalToConstant: 150),
-            movieImage.widthAnchor.constraint(equalToConstant: 100),
+    private func setupStackViewUI() {
+        labelStackView.addArrangedSubview(titleLabel)
+        labelStackView.addArrangedSubview(secondaryLabel)
+        contentView.addSubview(labelStackView)
 
-            // Stack View Constraints
+        NSLayoutConstraint.activate([
             labelStackView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            labelStackView.leadingAnchor.constraint(equalTo: movieImage.trailingAnchor, constant: 8),
+            labelStackView.leadingAnchor.constraint(equalTo: posterImageView.trailingAnchor, constant: 8),
             labelStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16)
         ])
     }
 
     func setupCellUI() {
         backgroundColor = .clear
-
     }
 
+    // MARK: - Configure methods
     func configure(with movie: Movie) {
-        titleLabel.text = movie.title
-        secondaryLabel.text = movie.releaseDate
-        setTestImage()
+        titleLabel.text = MovieHelper.shared.getMovieName(movie: movie)
+        secondaryLabel.text = MovieHelper.shared.getMovieReleaseDate(movie: movie)
+        posterImageView.setKFImage(with: movie.posterPath)
     }
-
-    func setTestImage() {
-        movieImage.kf.indicatorType = .activity
-        let url = URL(string: "https://www.carscoops.com/wp-content/uploads/2017/11/1c0f3a2c-vw-golf-r-performance-pack-upgrade-2.jpg") // swiftlint:disable:this line_length
-
-        KF.url(url)
-            .loadDiskFileSynchronously()
-            .cacheMemoryOnly()
-            .onFailure({ [weak self] error in
-                print(error.localizedDescription)
-                self?.movieImage.image = UIImage(named: "Filmatica-icon")
-            })
-            .fade(duration: 0.2)
-            .set(to: movieImage)
-
-    }
-
 }
